c5c62abcea1f89de066f6f5191c584a681cbc7e8
realpolya <ps2780@columbia.edu>
2023-04-16 11:25:45 -0400

    updating the website

--- projects/project02/main.js
+++ projects/project02/main.js
@@ -1,29 +1,126 @@
 console.log('this works')
 
+//size of the desert is measured in miles squared
 const deserts = [
     {
         name: "Desert1",
         location: "DeathValley",
-        size: 1000,
+        size: 5270,
         image: 'https://drive.google.com/uc?id=1iS6BFUnReISgARHlENTEYPeHw86XorJs'
     },
     {
         name: "Desert2",
         location: "Atacama",
-        size: 500,
+        size: 40441,
         image: 'https://drive.google.com/uc?id=1dLzd5MBeB-jpc52FHYPnnuQi9CWc031M'
     },
     {
         name: "Desert3",
         location: "DeathValley",
-        size: 1000,
+        size: 5270,
         image: 'https://drive.google.com/uc?id=1QljuAXmDr_cigiuFlf4JemHLRrBHqOUM'
     },
     {
         name: "Desert4",
         location: "Gobi",
-        size: 700,
+        size: 500000,
         image: 'https://drive.google.com/uc?id=18vYsRxljS3YkGABIIOJY5EqOeLdzLcvP'
+    },
+    {
+        name: "Desert5",
+        location: "Namib",
+        size: 31274,
+        image: 'https://drive.google.com/uc?id=1l1zB57qyjtFQwUXNd__vnp76OTGij-KX'
+    },
+    {
+        name: "Desert6",
+        location: "Namib",
+        size: 31274,
+        image: 'https://drive.google.com/uc?id=1tmAZ_PlwUSBNgvUVFoadsHuh60BD1cvh'
+    },
+    {
+        name: "Desert7",
+        location: "Namib",
+        size: 31274,
+        image: 'https://drive.google.com/uc?id=1kHsNlETjun4Hzzm5LRleOpgYgq0quN5i'
+    },
+    {
+        name: "Desert8",
+        location: "DeathValley",
+        size: 5270,
+        image: 'https://drive.google.com/uc?id=17fVbmCHWEkYgg3ssI25aXVJT0c35_GkC'
+    },
+    {
+        name: "Desert9",
+        location: "Atacama",
+        size: 40441,
+        image: 'https://drive.google.com/uc?id=1eHE5xb2F3ZpxjjOJMDL2yG9lgnTzZFMs'
+    },
+    {
+        name: "Desert10",
+        location: "Agafay",
+        size: 0.5,
+        image: 'https://drive.google.com/uc?id=1Vk2CL-Ay6WK1xXbsNBigee2FWnrJoGZ4'
+    },
+    {
+        name: "Desert11",
+        location: "Agafay",
+        size: 0.5,
+        image: 'https://drive.google.com/uc?id=1JR09jkXyU7XB0OvyZKkN4UPyHjJ_zu5V'
+    },
+    {
+        name: "Desert12",
+        location: "Agafay",
+        size: 0.5,
+        image: 'https://drive.google.com/uc?id=1vJy3ze3EwUq-DI5UbgJsdd8jjDeru9Ni'
+    },
+    {
+        name: "Desert13",
+        location: "Agafay",
+        size: 0.5,
+        image: 'https://drive.google.com/uc?id=18GSlj9iiuGlPk989eB5vvpn0UkHq4FWA'
+    },
+    {
+        name: "Desert14",
+        location: "Agafay",
+        size: 0.5,
+        image: 'https://drive.google.com/uc?id=1a48Fii7q8nKWbSATGUc_0cvxOruXp3Bp'
+    },
+    {
+        name: "Desert15",
+        location: "Gobi",
+        size: 500000,
+        image: 'https://drive.google.com/uc?id=1ySUC95_dRuzzOhufAI0qETRDjqor2Z7d'
+    },
+    {
+        name: "Desert16",
+        location: "Namib",
+        size: 31274,
+        image: 'https://drive.google.com/uc?id=1VXl9G0KSlR1auIiu4PGVSE2Ho6SBKptK'
+    },
+    {
+        name: "Desert17",
+        location: "Sahara",
+        size: 3552000,
+        image: 'https://drive.google.com/uc?id=18PbTxaKATX7QSbJxB9C8KWJCi2TJi7CU'
+    },
+    {
+        name: "Desert18",
+        location: "Sahara",
+        size: 3552000,
+        image: 'https://drive.google.com/uc?id=1efXBZOUyve7kgrhQyr5DEbU9obvnyJ5p'
+    },
+    {
+        name: "Desert19",
+        location: "Sahara",
+        size: 3552000,
+        image: 'https://drive.google.com/uc?id=1704yh3SdsfMTRj99IuNhrIrv_7O0Ji96'
+    },
+    {
+        name: "Desert20",
+        location: "Sahara",
+        size: 3552000,
+        image: 'https://drive.google.com/uc?id=1FkpuVk_GL-A4n-zOnml4t4Lq5fPRyHcF'
     },
 
 ]
@@ -59,6 +156,13 @@ function renderDesertToPage(results) {
 
 renderDesertToPage(deserts);
 
+//flipping the images to reveal their location
+//var flipping = document.querySelector('.flipping');
+//	flipping.addEventListener( 'click', function() {
+// 		flipping.classList.toggle('is-flipped');
+//	});
+
+
 // Sorting Method
 let filterBtns = document.querySelector(".filters");
 let cards = document.querySelectorAll(".card");
@@ -80,10 +184,92 @@ function sortingFn(event) {
             ul.innerHTML = ""
             renderDesertToPage(deserts)
         } else if (filterValue === 'from larger to smaller'){
-
+            deserts.sort(function(a, b){
+                if(a.size > b.size){
+                    return -1
+                }
+                if(a.size < b.size){
+                    return 1
+                }
+                return 0
+            })
             ul.innerHTML = ""
             renderDesertToPage(deserts)
         }
     }
 }
 filterBtns.addEventListener("click", sortingFn);
+
+//FILTERING BY LOCATION OF THE DESERT
+
+
+//Celeste's script exactly
+  
+let filterBtns1 = document.querySelector(".filtersdesert");
+let cards1 = document.querySelectorAll(".card");
+  
+  function filterFn(event) {
+    console.log(event.target)
+    if(event.target.classList.contains('filter-btn-desert')){
+      // select the current active button
+      let activeBtn = filterBtns1.querySelector('.active-desert')
+      activeBtn.classList.remove('active-desert')
+  
+      // apply the active class to the target
+      event.target.classList.add('active-desert')
+  
+      const filterValue1 = event.target.getAttribute('data-filter') // yellow | red
+  
+      for(let i = 0; i < cards.length; i++){
+        if(cards[i].classList.contains(filterValue1) || filterValue1 === 'all'){
+          cards[i].classList.remove('hide')
+          cards[i].classList.add('show')
+        } else {
+          cards[i].classList.remove('show')
+          cards[i].classList.add('hide')
+        }
+  
+      }
+    }
+  }
+  filterBtns1.addEventListener("click", filterFn);
+
+
+  //let filterBtns = document.querySelector(".filters");
+//let cards = document.querySelectorAll(".card");
+
+//let filterBtns1 = document.querySelector(".filters-desert");
+
+//function filterFn(event) {
+//  console.log(event.target)
+ //   if(event.target.classList.contains('filter-btn-desert')){
+ //       //select the current active button
+ //       let activeBtn = filterBtns1.querySelector('.active-desert')
+ //       activeBtn.classList.remove('active-desert')
+//
+        //apply the active class to the target
+ //       event.target.classList.add('active-desert')
+
+ //       const filterValue1 = event.target.getAttribute('data-filter') // name of the location
+
+ //       for(let i = 0; i < cards.length; i++){
+ //           if(cards[i].classList.contains(filterValue1) || filterValue1 === 'all'){
+//                cards[i].classList.remove('hide')
+ //               cards[i].classList.add('show')
+ //           } else {
+ //               cards[i].classList.remove('show')
+ //               cards[i].classList.add('hide')
+ //           }
+ //           
+ //       }
+ //   }
+//}
+
+//filterBtns1.addEventListener("click", filterFn)
+
+//what is wrong with the script?
+
+//const saharaDesertImages =
+//    deserts.filter(function(deserts) {
+ //   return deserts.location == "Sahara";
+//});
